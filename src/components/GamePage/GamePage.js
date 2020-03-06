0f596e18e3760a154ae8efcae9af7c887c75ab94
liber.roi <liber.roi@gmail.com>
2020-03-07 00:29:48 +0200

    game board set

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector } from "react-redux";
 import './style.scss';
 
 export default function Home() {
     const newGameData = useSelector(state => state.mainReducer.newGameData);
+    const [gameLines, setGameLines] = useState([
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', ''],
+    ]);
+
+    useEffect(() => {
+        const squares = newGameData.squares;
+        let newGameLines = [...gameLines];
+        console.log(squares);
+        squares.map(item => {
+            newGameLines[item.y].splice(item.x, 1, item.x)
+        });
+        setGameLines(newGameLines);
+    }, [newGameData]);
 
     return (
         <div className={'game_wrapper'}>
             <div className={'game_table'}>
-                {newGameData.squares.map((item, index) => {
-                    return <div key={index} className={'square'}>{item.value}</div>
+                {gameLines.map((item, index) => {
+                    return <div key={index} className={'line'}>
+                        {item.map((square, index) => {
+                            return <span key={index} className={'square'}>{square}</span>
+                        })}
+                    </div>
                 })}
             </div>
         </div>
