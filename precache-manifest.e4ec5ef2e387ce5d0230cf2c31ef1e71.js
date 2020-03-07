aa83c3c003ae9fe06f982b03ddb35cec109a79a4
liber.roi <liber.roi@gmail.com>
2020-03-07 19:08:20 +0200

    Updates

---
+++ precache-manifest.e4ec5ef2e387ce5d0230cf2c31ef1e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261d2825d938e9e5077e53345cb86b78",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "d8bb1419896e3264c600",
+    "url": "/Sudoku/static/css/main.107eae8d.chunk.css"
+  },
+  {
+    "revision": "2fbfafa61b0fd5eccadc",
+    "url": "/Sudoku/static/js/2.de808834.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Sudoku/static/js/2.de808834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bb1419896e3264c600",
+    "url": "/Sudoku/static/js/main.a8911a5d.chunk.js"
+  },
+  {
+    "revision": "52c8538967182f0b5275",
+    "url": "/Sudoku/static/js/runtime-main.f3d39b8e.js"
+  }
+]);
