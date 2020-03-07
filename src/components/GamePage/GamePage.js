27f2bcf9eace07226c179a62d520663cd86db7fa
liber.roi <liber.roi@gmail.com>
2020-03-07 18:57:33 +0200

    game done

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -10,6 +10,7 @@ export default function Home() {
     const [dragDice, setDragDice] = useState('');
     const [dropLineAndIndex, setDropLineAndIndex] = useState({});
     const [isDiceValid, setIsDiceValid] = useState(true);
+    const [isGameDone, setIsGameDone] = useState(false);
 
     useEffect(() => {
         setTable()
@@ -41,7 +42,23 @@ export default function Home() {
         setGameTable(newGameLines)
     }
 
+    function validateDoneGame() {
+        function checkClearItems() {
+            let isEmpty = false;
+            gameTable.map(row => {
+                return row.map(item => {
+                    if (!item) isEmpty = true
+                })
+            });
+            return isEmpty
+        }
+        const isGameTableDone = !checkClearItems() && isDiceValid;
+        setIsGameDone(isGameTableDone)
+    }
+
     function diceValidation(line, lineIndex) {
+        let square = creatSquare(lineIndex, line);
+
         gameTable[line].map((item, index) => {
             if (lineIndex !== index && dragDice === Number(item)) {
                 setIsDiceValid(false)
@@ -52,6 +69,94 @@ export default function Home() {
                 setIsDiceValid(false)
             }
         });
+        square.map(item => {
+            if (dragDice === Number(item)) {
+                setIsDiceValid(false)
+            }
+        });
+        validateDoneGame()
+    }
+
+    function creatSquare(rowIndex, rowNumber) {
+        let newLineArray = [];
+
+        function pushToArray(rows, indexArray) {
+            rows.map(row => {
+                indexArray.map(item => {
+                    if (row === rowNumber && item === rowIndex) {return false}
+                    else {
+                        newLineArray.push(gameTable[row][item]);
+                    }
+                })
+            })
+        }
+
+        switch (rowNumber) {
+            case 0: case 1: case 2: {
+                switch (rowIndex) {
+                    case 0: case 1: case 2: {
+                        pushToArray([0, 1, 2], [0, 1, 2]);
+                        break
+                    }
+                    case 3: case 4: case 5: {
+                        pushToArray([0, 1, 2], [3, 4, 5]);
+                        break
+                    }
+                    case 6: case 7: case 8: {
+                        pushToArray([0, 1, 2], [6, 7, 8]);
+                        break
+                    }
+                    default: {
+                        return newLineArray
+                    }
+                }
+                break
+            }
+            case 3: case 4: case 5: {
+                switch (rowIndex) {
+                    case 0: case 1: case 2: {
+                        pushToArray([3, 4, 5], [0, 1, 2]);
+                        break
+                    }
+                    case 3: case 4: case 5: {
+                        pushToArray([3, 4, 5], [3, 4, 5]);
+                        break
+                    }
+                    case 6: case 7: case 8: {
+                        pushToArray([3, 4, 5], [6, 7, 8]);
+                        break
+                    }
+                    default: {
+                        return newLineArray
+                    }
+                }
+                break
+            }
+            case 6: case 7: case 8: {
+                switch (rowIndex) {
+                    case 0: case 1: case 2: {
+                        pushToArray([6, 7, 8], [0, 1, 2]);
+                        break
+                    }
+                    case 3: case 4: case 5: {
+                        pushToArray([6, 7, 8], [3, 4, 5]);
+                        break
+                    }
+                    case 6: case 7: case 8: {
+                        pushToArray([6, 7, 8], [6, 7, 8]);
+                        break
+                    }
+                    default: {
+                        return newLineArray
+                    }
+                }
+                break
+            }
+            default: {
+                return newLineArray
+            }
+        }
+        return newLineArray
     }
 
     function clearLastDice() {
@@ -66,7 +171,7 @@ export default function Home() {
             <div className={'game_dice'}>
                 {dices.map((item, index) => {
                     return <span key={index}
-                                 className={'dice'}
+                                 className={`dice ${isDiceValid ? 'pointer' : ''}`}
                                  draggable={isDiceValid}
                                  onDragStart={(event) => onDragStart(event, index+1)}
                     >{item}</span>
@@ -98,6 +203,24 @@ export default function Home() {
                         onClick={() => clearLastDice()}
                     >
                         Clear Last Dice
+                    </Button>
+                </div>}
+            {isGameDone &&
+                <div className={'clear_button_wrapper'}>
+                    <span>Nice You Did It</span>
+                    <Button
+                        className={'clear_table'}
+                        variant="text"
+                        onClick={() => {}}
+                    >
+                        Done
+                    </Button>
+                    <Button
+                        className={'clear_table'}
+                        variant="text"
+                        onClick={() => {}}
+                    >
+                        Play Again
                     </Button>
                 </div>}
         </div>
