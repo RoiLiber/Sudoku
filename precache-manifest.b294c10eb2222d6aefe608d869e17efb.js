d419a0fe922a44e69017cfd49f63335bea34b9b6
liber.roi <liber.roi@gmail.com>
2020-03-08 07:34:42 +0200

    Updates

---
+++ precache-manifest.b294c10eb2222d6aefe608d869e17efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f00c346fa3efc4b35294bc6d89c8f5",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "7f1f203462a0b5c7f810",
+    "url": "/Sudoku/static/css/main.ac073857.chunk.css"
+  },
+  {
+    "revision": "90774f64407a973fa1bd",
+    "url": "/Sudoku/static/js/2.3930caed.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Sudoku/static/js/2.3930caed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1f203462a0b5c7f810",
+    "url": "/Sudoku/static/js/main.684edacc.chunk.js"
+  },
+  {
+    "revision": "52c8538967182f0b5275",
+    "url": "/Sudoku/static/js/runtime-main.f3d39b8e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sudoku/precache-manifest.e4ec5ef2e387ce5d0230cf2c31ef1e71.js"
+  "/Sudoku/precache-manifest.b294c10eb2222d6aefe608d869e17efb.js"
 );
 
 self.addEventListener('message', (event) => {
