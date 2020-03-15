c66000e37172c0f8fc38ec71ba5866926bc31ac7
liber.roi <liber.roi@gmail.com>
2020-03-15 20:40:26 +0200

    score remove done

--- src/components/ScorePage/ScorePage.js
+++ src/components/ScorePage/ScorePage.js
@@ -1,68 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NO_SCORES } from '../../consts';
 import './style.scss';
 
 export default function ScorePage() {
     const scoresTable = localStorage.getItem('scoresTable');
     const scores = JSON.parse(scoresTable);
+    const [scoresList, setScoresList] = useState(orderScores(scores));
 
-    function orderScores() {
+    function orderScores(scores) {
         let newOrderScores = [];
         let newOrderScore = [];
 
-        scores.map((item, index) => {
+        scores && scores.map((item, index) => {
             const filByMin = scores.filter(time => time.minutes === item.minutes);
 
             if (index === 0) {
                 newOrderScores.push(filByMin);
             }
             else {
-                const lastItem = newOrderScores[newOrderScores.length - 1];
-
-                if (filByMin.length === lastItem.length) { return false }
-                else {
-                    newOrderScores.push(filByMin);
-                }
+                const isFind = newOrderScores.find(timeArray => timeArray[0].minutes === item.minutes)
+                !isFind && newOrderScores.push(filByMin);
             }
         });
-        newOrderScores.map(item => {
+
+        const orderScoresByMinutes = newOrderScores.sort((a, b) => a[0].minutes - b[0].minutes);
+
+        scores && orderScoresByMinutes.map(item => {
             const orderScoresBySeconds = item.sort((a, b) => a.seconds - b.seconds);
+
             newOrderScore.push(...orderScoresBySeconds);
         });
         return newOrderScore
     }
 
     function clearResult(score) {
-        // const newScoreList = scores.find(item => item.clockMin === score.minutes && item.clockSec === score.seconds);
-        //
-        // localStorage.setItem('scoresTable', JSON.stringify(newScoreList));
+        const filByMin = scoresList.filter(time => time.minutes !== score.minutes);
+        const filBySec = scoresList.filter(time => time.minutes === score.minutes);
+        const remove = filBySec.filter(time => time.seconds !== score.seconds);
+        const newScoreList = [...filByMin, ...remove];
+
+        localStorage.setItem('scoresTable', JSON.stringify(newScoreList));
+        setScoresList(orderScores(newScoreList))
     }
 
     return (
         <div className="score_page">
             <span className={'title'}>score page</span>
             <div className={'scores_wrapper'}>
-                {scores !== null
-                    ?  orderScores().map((score, index) => {
-                          const seconds = score.seconds;
-                          const minutes = score.minutes;
-                          const clockSec = seconds < 10 ? `0${seconds}` : seconds;
-                          const clockMin = minutes < 10 ? `0${minutes}` : minutes;
-
-                          return <div className={'score_wrapper'} key={index}>
-                              <span className={'table_position'}>{index + 1}.</span>
-                              <span className={'time'}>
-                              {clockMin}:{clockSec}
-                                  <i className="far fa-times-circle"
-                                   onClick={() => clearResult(score)}
-                                  />
-                          </span>
-                          </div>
-                    })
-                    : <div className={'no_scores'}>
+                {!scoresList.length
+                    ?  <div className={'no_scores'}>
                         <span>{NO_SCORES[0]}</span>
                         <span>{NO_SCORES[1]}</span>
-                    </div>}
+                    </div>
+                    : scoresList.map((score, index) => {
+                        const seconds = score.seconds;
+                        const minutes = score.minutes;
+                        const clockSec = seconds < 10 ? `0${seconds}` : seconds;
+                        const clockMin = minutes < 10 ? `0${minutes}` : minutes;
+
+                        return <div className={'score_wrapper'} key={index}>
+                            <span className={'table_position'}>{index + 1}.</span>
+                            <span className={'time'}>
+                              {clockMin}:{clockSec}
+                                <i className="far fa-times-circle"
+                                   onClick={() => clearResult(score)}
+                                />
+                          </span>
+                        </div>
+                    })}
             </div>
         </div>
     );
